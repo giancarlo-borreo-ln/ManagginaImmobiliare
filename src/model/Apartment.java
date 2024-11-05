133afe1d97898d7bddd1856625013352aa62dfde
giancarlo <giancarlo@mail.it>
2024-11-05 10:52:55 +0100

    Started creating models

---
+++ src/model/Apartment.java
@@ -0,0 +1,94 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Apartment {
+    //VERBOTEN
+    private int id;
+    //Numero + lettera, numero indica piano e lettera è A o B
+    private String number;
+    private double area;
+    //Relazione verso padre condominio
+    private Condominium condominium;
+    private int condominium_id;
+    //Relazione verso figlio unico garage
+    private Garage garage;
+    //Relazione verso i figli tenants che saranno gli inquillini
+    private ArrayList<Tenant> tenants = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if(id<1)
+            throw new RuntimeException();
+
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        String lettera = number.substring(number.length()-1);
+        int numero = Integer.parseInt(number.substring(0, number.length()-1));
+        if((!lettera.equals("A") && !lettera.equals("B")) || numero < 1 || numero > condominium.getFloors())
+            throw new RuntimeException();
+        this.number = number;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        if(area<0)
+            throw new RuntimeException();
+
+        this.area = area;
+    }
+
+    public Condominium getCondominium() {
+        return condominium;
+    }
+
+    public void setCondominium(Condominium condominium) {
+        if(condominium==null)
+            throw new RuntimeException();
+        this.condominium = condominium;
+        condominium.addApartment(this);
+        condominium_id = condominium.getId();
+
+    }
+
+    public int getCondominium_id() {
+        return condominium_id;
+    }
+
+    public void setCondominium_id(int condominium_id) {
+        if(condominium_id<1)
+            throw new RuntimeException();
+        this.condominium_id = condominium_id;
+    }
+
+    public ArrayList<Tenant> getTenants() {
+        return tenants;
+    }
+
+
+    public void addTenants(Tenant tenant) {
+        tenants.add(tenant);
+    }
+
+    public Garage getGarage() {
+        return garage;
+    }
+
+    public void setGarage(Garage garage) {
+        if(garage==null)
+            throw new RuntimeException();
+        this.garage = garage;
+    }
+}
